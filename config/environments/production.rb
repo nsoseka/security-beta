35175e4436aa8b54ebd3cc79484a28b67652f160
nsoseka <lisaacjunior@yahoo.com>
2018-06-13 03:35:44 +0100

    storage probs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Store files on Amazon S3.
   config.active_storage.service = {
-    service: S3,
+    service: 'S3',
     access_key_id: ENV["s3_access_key_id"],
     secret_access_key: ENV["s3_secret_access_key"],
     bucket: ENV["s3_bucket"],
