79996347364b2abac7df8ccad676955426f13a0a
nsoseka <lisaacjunior@yahoo.com>
2018-06-13 02:54:55 +0100

    agents issues

--- db/migrate/20180606092421_fix_up_agents_table.rb
+++ db/migrate/20180606092421_fix_up_agents_table.rb
@@ -2,7 +2,7 @@ class FixUpAgentsTable < ActiveRecord::Migration[5.1]
   def change
     add_column :agents, :full_name, :string
     add_column :agents, :email, :string
-    add_column :agents, :phone_number, :number 
+    add_column :agents, :phone_number, :string
     add_column :agents, :password_digest, :string
     add_column :agents, :verification_status, :boolean, default: false
     add_column :agents, :email_verification_status, :boolean, default: true
